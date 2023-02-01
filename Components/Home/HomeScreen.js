b014c948fb1b9b42b26216a1fb212c97399316c7
kamenan12 <kamenanantoine@gmail.com>
2023-02-01 11:26:02 +0000

    Initialisation du projet client

---
+++ Components/Home/HomeScreen.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet } from "react-native";
+
+
+
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text> Bonnnnnnaaane ee </Text>
+        </View>
+    )
+}
+
+
+
+
+export default HomeScreen ;
+
+
+const styles = StyleSheet.create({
+    
+})
---
+++ Test/test.js
@@ -0,0 +1,20 @@
+import { View, Text} from "react-native"
+
+
+
+const test = () => {
+    return (
+        <View>
+            <Text>
+                tetestts
+            </Text>
+        </View>
+    )
+}
+
+
+export default test;
+
+const styles = StyleSheet.create({
+    
+})
