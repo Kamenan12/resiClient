f92ce95ddd27aaf6562312f397acae59316d6aa3
kamenan12 <kamenanantoine@gmail.com>
2023-02-08 20:43:16 +0000

    ajout des top commune effectifs

--- Components/Home/TopVille.js
+++ Components/Home/TopVille.js
@@ -1,4 +1,8 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Ville } from "../Data/Ville";
+
+import tw from "twrnc"
+import { ScrollView } from "react-native";
 
 
 
@@ -7,10 +11,22 @@ const TopVille = () => {
 
 
     return (
-        <View>
-            <Text>
-                Top ville
-            </Text>
+        <View style={tw`px-2`}>
+            <View style={tw`flex-row justify-between`}>
+                <Text style={{ fontSize: 15, fontWeight: "700"}}> Top Villes</Text>
+                <Text> Tous </Text>
+            </View>
+
+            <View style={tw`flex-row `}>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+
+                {
+                    Ville.map( (V, index) => (
+                        <VilleTop ville={V} index={index}/>
+                    ))
+                }
+                </ScrollView>
+            </View>
         </View>
     )
 }
@@ -19,10 +35,29 @@ const TopVille = () => {
 
 
 
+
+const VilleTop = (props) => {
+    const V = props.ville
+    return (
+        <View style={tw`px-2 py-2`}>
+            <View style={tw`flex-row p-2  bg-white rounded-lg shadow-lg`}>
+                <View style={tw`p-1 bg-gray-300 rounded-lg`}>
+                    <Image source={V.Image} style={{ width: 30, height: 30}}/>
+                </View>
+                <View style={tw`justify-center px-1`}>
+                 <Text style={{fontWeight: "500", fontSize: 15}}>{V.nom}</Text>
+                </View>
+            </View>
+            </View>
+    )
+}
+
+
+
 export default TopVille; 
 
 
 
 const styles = StyleSheet.create({
-    
+
 })
