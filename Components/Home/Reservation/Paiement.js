4e6d3cae789034a6cf8225c83551cb6f5c491d65
kamenan12 <kamenanantoine@gmail.com>
2023-03-02 16:49:35 +0000

    Essayons D'ajouter le reCatchap Invisible pour la connexion avec le numero de telephone

--- Components/Home/Reservation/Paiement.js
+++ Components/Home/Reservation/Paiement.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { ChoixPaiement } from "../../Data/Paiement";
 import { Icon } from "@rneui/themed";
 import tw from "twrnc"
@@ -37,18 +37,20 @@ const Paiement = () => {
 const ModePaiement = (props) => {
     const methode = props.methode
     return (
-        <View style={tw`bg-white py-3 w-86 h-20 justify-between px-7 items-center mb-2 flex-row rounded-lg`}>
-            <View>
-                <Image source={methode.logo} style={tw`w-15 h-15`}/>
+        <TouchableOpacity >
+            <View style={tw`bg-white py-3 w-86 h-20 justify-between px-7 items-center mb-2 flex-row rounded-lg`}>
+                <View>
+                    <Image source={methode.logo} style={tw`w-15 h-15`}/>
+                </View>
+                <View>
+                    <Text style={[{fontSize: 20, fontWeight: "600"}]}> {methode.nom}</Text>
+                    <Text style={{color: "gray"}}> {methode.frais}%, Frais operateur</Text>
+                </View>
+                <View>
+                    <Icon type="antdesign" name="rightcircleo" color="red"/>
+                </View>
             </View>
-            <View>
-                <Text style={[{fontSize: 20, fontWeight: "600"}]}> {methode.nom}</Text>
-                <Text style={{color: "gray"}}> {methode.frais}%, Frais operateur</Text>
-            </View>
-            <View>
-                <Icon type="antdesign" name="rightcircleo" color="red"/>
-            </View>
-        </View>
+        </TouchableOpacity>
     )
 }
 
