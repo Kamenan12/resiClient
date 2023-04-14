80ba7d3ef4565215f0c34221bb5bda64338ae546
kamenan12 <kamenanantoine@gmail.com>
2023-04-14 22:38:11 +0000

    Ajout du code de recuperation de l'user connecter pour drawer

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerI
 // import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Icon } from '@rneui/themed';
 import { auth } from './firebase';
+// import { getAuth } from 'firebase/auth';
 import { db } from './firebase';
 import { collection, getDocs, where, query, onSnapshot } from "firebase/firestore";
 import tw from "twrnc"
@@ -27,8 +28,7 @@ import Login from './Components/connexion/Login';
 
 
 
-
-
+ 
 const HomeStack = createNativeStackNavigator()
 
   function HomeScreen() {
@@ -39,11 +39,50 @@ const HomeStack = createNativeStackNavigator()
     )
   }
 
-  function LienDrawerContent (props) {
-    
-    return (
+  function LienDrawerContent (props) { 
+
+    // const user = auth.currentUser
+    const [userDoc, setUserDoc] = useState(); 
+
+    // const getUserDoc = async() => {
+    //   if (user) {
+    //     const q = query(collection(db, "users"), where("user", "==", user.uid));
+    //     const unsubscribre = onSnapshot(q, (querySnapshot) => {
+    //     const dc = [];
+    //     querySnapshot.forEach((doc) => {
+    //       dc.push(doc.data())
+    //       console.log("les doccc", dc)
+    //     });
+    //     setUserDoc(dc[0])
+    //   })
+    //   } else {
+    //     console.log("passs user")
+    //   }
+      
+      
+    // } 
+
+    useEffect(() => { 
+      const unsubscribe = auth.onAuthStateChanged(user => {
+        if (user) {
+            // console.log("user la", user.uid)
+            const q = query(collection(db, "users"), where("user", "==", user.uid));
+                const unsubscribre = onSnapshot(q, (querySnapshot) => {
+                const dc = [];
+                querySnapshot.forEach((doc) => {
+                  dc.push(doc.data())
+                  console.log("les doccc", dc)
+                });
+                setUserDoc(dc[0])
+              })
+        }
+    })
+    return unsubscribe
+      // getUserDoc();
+  }, [])
+    return ( 
       <DrawerContentScrollView {...props} >
-        <DrawerItem label="Seting" onPress={() => props.navigation.navigate("Setting")} style={{ borderBottomWidth: 1, borderBottomColor: "gray", backgroundColor: "white"}} 
+        <DrawerItem label={userDoc ? userDoc.nom : "setiing"} onPress={() => props.navigation.navigate("Setting")} style={{ borderBottomWidth: 1, borderBottomColor: "gray", backgroundColor: "white"}} 
         labelStyle={{ fontSize: 18, fontWeight: "800", fontFamily: "serif"}}
         icon={() => <Icon name='user' type='evilicon' color="black" size={50} />} />
         {/* <DrawerItem label="Pappa" onPress={() => alert('Link papapa')} /> */}
@@ -88,27 +127,37 @@ const Stack = createNativeStackNavigator();
 
 
 export default function App() {
-  const user = auth.currentUser;
-  const [userDoc, setUserDoc] = useState();
-  const getUserDoc = () => {
-    const q = query(collection(db, "users"), where("user", "==", user.uid))
-    const unsubscribre = onSnapshot(q, (querySnapshot) => {
-      const dc = [];
-      querySnapshot.forEach((doc) => {
-        dc.push(doc.data())
-        console.log("les doccc", dc)
-      });
-      setUserDoc(dc[0])
-    })
-  }
+  // const user = auth.currentUser
+  // const [userDoc, setUserDoc] = useState(); 
 
-  useEffect(() => {
-    getUserDoc();
-}, [])
+  //   const getUserDoc = async() => {
+  //     // console.log("user crr", user.uid)
+  //     if (user) {
+  //       const q = query(collection(db, "users"), where("user", "==", user.uid));
+  //       const unsubscribre = onSnapshot(q, (querySnapshot) => {
+  //       const dc = [];
+  //       querySnapshot.forEach((doc) => {
+  //         dc.push(doc.data())
+  //         console.log("les doccc", dc)
+  //       });
+  //       setUserDoc(dc[0])
+  //     })
+  //     } else {
+  //       console.log("passs user")
+  //     }
+      
+      
+  //   } 
+
+  //   useEffect(() => { 
+      
+  //     getUserDoc();
+  // }, [])
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home-G'>
-        <Stack.Screen name="Home-G" component={DrawerNavigation} options={{ headerShown: false, }}/>
+        <Stack.Screen name="Home-G" component={DrawerNavigation} options={{ headerShown: false, }} />
         <Stack.Screen name="Details" component={DetailView} options={{ headerShown: false, }}/>
         <Stack.Screen name="Reservation" component={Reservation} options={{ headerShown: false, }}/>
         <Stack.Screen name="Paiement" component={Paiement} options={{ headerShown: false, }}/>
