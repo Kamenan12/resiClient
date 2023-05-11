3006959a06ca784cdd0482aafc6f2d549916c0ff
kamenan12 <kamenanantoine@gmail.com>
2023-05-11 01:03:44 +0000

    recuperation des reservations des user connecte

--- Components/MesReservation/MesReservation.js
+++ Components/MesReservation/MesReservation.js
@@ -1,11 +1,35 @@
 import { View, Text, StyleSheet } from "react-native";
 
+import { QuerySnapshot, collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useSelector, useDispatch } from "react-redux";
 import tw from "twrnc"
+import { useEffect } from "react";
 
 
 
 
 const MesReservation = () => {
+    const idUser = useSelector((state) => state.user.user)
+    // console.log("user", idUser)
+    const getMesResidence = async() => {
+        let q = query(collection(db, "reservations"), where("user", "==", idUser))
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const r = [];
+            querySnapshot.forEach((doc) => {
+                // console.log("data", doc.data())
+                r.push(doc.data())
+            })
+            console.log("mesReservation", r)
+        })
+    }
+
+
+    useEffect(()=> {
+        // console.log("user efefefefe");
+        getMesResidence()
+    }, [])
+
     return (
         <>
             <View style={tw`items-center justify-center h-full`}>
